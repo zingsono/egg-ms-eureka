078a693cf0794477162cf3d0b318ee30fbe79b09
Zengs <zingsono@gmail.com>
2019-08-05 18:06:08 +0800

    1.0.5 fix 异常处理

--- app/extend/application.js
+++ app/extend/application.js
@@ -32,7 +32,7 @@ module.exports = {
             }
             return data;
         } catch (e) {
-            if (e.message.code) {
+            if (e.code) {
                 throw e;
             }
             this.logger.error(e);
